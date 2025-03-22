6611c6e8927acfa1d6bcb22773d2c24f758e3967
fredatar <teslenko.70@mail.ru>
2025-03-22 14:33:13 +0300

    Доработка методв GET & POST, добавлен logger

--- internal/server/http/http_server/http_server.go
+++ internal/server/http/http_server/http_server.go
@@ -2,6 +2,8 @@ package httpserver
 
 import (
 	"net/http"
+
+	"github.com/Valery223/ServerTestingLab/internal/logger"
 )
 
 type HTTPServer struct {
@@ -11,13 +13,17 @@ type HTTPServer struct {
 func (hs *HTTPServer) Init() {
 	mu := http.NewServeMux()
 	userHandler := UserHandler{}
-	mu.HandleFunc("GET /user", userHandler.GetUser)
+	handleGetUser := http.HandlerFunc(userHandler.GetUser)
+	handleGetUser = logginMiddleWare(handleGetUser)
+	mu.HandleFunc("GET /user", handleGetUser)
 	mu.HandleFunc("POST /user", userHandler.PostUser)
 
 	hs.server = http.Server{Addr: ":8081", Handler: mu}
+	logger.Logger.Info("Server announced", "addr", hs.server.Addr)
 }
 
 func (hs *HTTPServer) Run() error {
+	logger.Logger.Info("Server running", "addr", hs.server.Addr)
 	err := hs.server.ListenAndServe()
 	return err
 }
--- internal/server/http/http_server/userHandle.go
+++ internal/server/http/http_server/userHandle.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/Valery223/ServerTestingLab/internal/logger"
 )
 
 type UserHandler struct{}
 
+// Echo ответ, получая имя из header "Name"
 func (uh *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		// Дренирование
@@ -15,22 +18,61 @@ func (uh *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
 		r.Body.Close()
 	}()
 
-	_, _ = w.Write([]byte("Hello World"))
+	name := r.Header.Get("Name")
+	if name != "" {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		n, err := w.Write([]byte(fmt.Sprintf("Received header Name: %s", name)))
+		if err != nil {
+			logger.Logger.Error("request progress",
+				"method", r.Method,
+				"path", r.URL.Path,
+				"remote_addr", r.RemoteAddr,
+				"error", err)
+		} else {
+			logger.Logger.Debug("request progress",
+				"method", r.Method,
+				"path", r.URL.Path,
+				"remote_addr", r.RemoteAddr,
+				"bytes_sent", n)
+		}
+	} else {
+		logger.Logger.Warn("request progress",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"remote_addr", r.RemoteAddr,
+			"error", "name not find")
+
+		// В ручную сначала хочу отправлять ошибку, для обучения
+		// http.Error(w, "Name header is required", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Name header is required"))
+	}
 }
 
+// Логирование body, полученного из запроса
 func (uh *UserHandler) PostUser(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		_, _ = io.Copy(io.Discard, r.Body)
 		r.Body.Close()
 	}()
 
-	name := r.Header.Get("Name")
-	// Если значение не пустое, добавляем его в заголовки ответа под корректным названием
-	if name != "" {
-		w.Header().Add("Echo", name)
-		// Также можно вернуть значение в теле ответа
-		_, _ = w.Write([]byte(fmt.Sprintf("Received header Name: %s", name)))
-	} else {
-		http.Error(w, "Header 'Name' is required", http.StatusBadRequest)
+	switch r.Header.Get("Content-Type") {
+	case "text/plain":
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Logger.Error("get error", "error", err)
+		} else {
+			logger.Logger.Info("read from body",
+				"body", string(body))
+		}
+	default:
+		logger.Logger.Warn("request progress",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"remote_addr", r.RemoteAddr,
+			"error", "Unsupported media type")
+		http.Error(w, "Unsupported media type", http.StatusUnsupportedMediaType)
+
 	}
 }
