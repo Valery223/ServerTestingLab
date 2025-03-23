c08cf31aba307f86d24bcb994cc633b78cc7ef97
Valery <teslenko.70@mail.ry>
2025-03-23 23:51:22 +0300

    Добавил tls (https)

--- cmd/http-server/ISSUES.md
+++ cmd/http-server/ISSUES.md
@@ -1,18 +1,24 @@
 ## План на http серсер
-- Реализовать методы к /user
-    - GET, POST, OPTION
-    - Правильые статус коды ответов
-    - Хорошая маршрутизация
+- +Реализовать методы к /user 
+    - GET, POST, OPTION +
+    - Правильые статус коды ответов +
+    - Хорошая маршрутизация +
 - Наастроит логи
     - Иницилизацию slog
     - Middleware-log обертки над запросами 
+    - Как-то сунуть свой логер в net/http Server(а то он свой использует)
+        - "http: TLS handshake error from 127.0.0.1:58092: read tcp 127.0.0.1:8081->127.0.0.1:58092: i/o timeout"
 - Тесты
     - Изучить http/httptest
 
-- Настройка сервера
+- +Настройка сервера
     - Реализовать Grasefull shutdown
     - KeepALive,  TimeToReadHead etc
     - TLS (HTTPS)
+        -генерирует сертификат и ключ: 
+        ```bash
+        openssl req -x509 -newkey rsa:4096 -nodes -keyout localhost.key -out localhost.crt -days 365 -subj "/CN=localhost"
+        ```
 
 - Отправка файлов и HTTP/2.0
     - Сделать index.html и соответствующие ему css файлы
--- internal/server/http/http_server/http_server.go
+++ internal/server/http/http_server/http_server.go
@@ -3,6 +3,7 @@ package httpserver
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/Valery223/ServerTestingLab/internal/logger"
 )
@@ -13,19 +14,30 @@ type HTTPServer struct {
 
 func (hs *HTTPServer) Init() {
 	mu := http.NewServeMux()
+
 	userHandler := UserHandler{}
+	//UserHandlers
 	handleGetUser := http.HandlerFunc(userHandler.GetUser)
 	handleGetUser = logginMiddleWare(handleGetUser)
 	mu.HandleFunc("GET /user", handleGetUser)
 	mu.HandleFunc("POST /user", userHandler.PostUser)
 
-	hs.server = http.Server{Addr: ":8081", Handler: mu}
+	//StaticHandlers
+	fs := http.FileServer(http.Dir("./static"))
+	handleFs := logginMiddleWare(http.StripPrefix("/static/", fs).ServeHTTP)
+	mu.Handle("/static/", handleFs)
+
+	hs.server = http.Server{Addr: ":4443",
+		Handler:           mu,
+		ReadHeaderTimeout: 3 * time.Second, // Random)
+		IdleTimeout:       5 * time.Minute, // Again random:)
+	}
 	logger.Logger.Info("Server announced", "addr", hs.server.Addr)
 }
 
 func (hs *HTTPServer) Run() error {
 	logger.Logger.Info("Server running", "addr", hs.server.Addr)
-	return hs.server.ListenAndServe()
+	return hs.server.ListenAndServeTLS("localhost.crt", "localhost.key")
 
 }
 
